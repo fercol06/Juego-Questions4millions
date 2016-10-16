a9cccd37b2ee98eb30b018611dd8f9a28f33ce43
Javi <javi.fernandez@opendeusto.es>
2016-10-16 13:04:12 +0200

    VentanaConfiguracion v0.0.1

---
+++ Proyecto Prog 3 Juego Preguntas/src/ventanas/VentanaConfiguracion.java
@@ -0,0 +1,48 @@
+package ventanas;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class VentanaConfiguracion extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaConfiguracion frame = new VentanaConfiguracion();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaConfiguracion() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel_norte = new JPanel();
+		contentPane.add(panel_norte, BorderLayout.NORTH);
+		
+		JPanel panel_sur = new JPanel();
+		contentPane.add(panel_sur, BorderLayout.SOUTH);
+	}
+
+}
