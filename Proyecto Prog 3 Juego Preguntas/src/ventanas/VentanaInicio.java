58d6b23993e347d7c51e178882b1b452da4ece71
Alejandro Ron <alejandro.ron@opendeusto.es>
2016-10-24 15:21:21 +0200

    v0.0.1

--- Proyecto Prog 3 Juego Preguntas/src/ventanas/VentanaInicio.java
+++ Proyecto Prog 3 Juego Preguntas/src/ventanas/VentanaInicio.java
@@ -1,4 +1,5 @@
 package ventanas;
+
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
@@ -13,32 +14,30 @@ import java.awt.BorderLayout;
 import javax.swing.JPanel;
 import javax.swing.ImageIcon;
 
-public class VentanaInicio extends JFrame implements ActionListener{
+public class VentanaInicio extends JFrame implements ActionListener {
 
-	private JFrame frame;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel panel_centro = new JPanel();
 	private JButton btnJugar = new JButton("Jugar");
 	private JButton btnMarcadores = new JButton("Marcadores");
 	private JButton btnInstrucciones = new JButton("Instrucciones");
 	private JButton btnSalir = new JButton("Salir");
-	private JPanel panel = new JPanel();
 
-
-
-
-
-
-    
 	/**
-	 * Launch the application.
-	 * Ventana Principal.
+	 * Launch the application. Ventana Principal.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					VentanaInicio window = new VentanaInicio();
-					window.frame.setVisible(true);
+					VentanaInicio frame = new VentanaInicio();
+					frame.setSize(640, 640);
+					frame.setVisible(true);
+					frame.setTitle("Questions4millions");
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -50,27 +49,7 @@ public class VentanaInicio extends JFrame implements ActionListener{
 	 * Create the application.
 	 */
 	public VentanaInicio() {
-		
-		getContentPane().setLayout(new BorderLayout(0, 0));
-		
-		getContentPane().add(panel_centro, BorderLayout.CENTER);
-		panel_centro.setLayout(null);
-		
-		btnJugar.setBounds(47, 154, 150, 29);
-		panel_centro.add(btnJugar);
-		
-		btnMarcadores.setBounds(47, 199, 150, 29);
-		panel_centro.add(btnMarcadores);
-		
-		btnInstrucciones.setBounds(212, 154, 150, 29);
-		panel_centro.add(btnInstrucciones);
-		
-		btnSalir.setBounds(212, 199, 150, 29);
-		panel_centro.add(btnSalir);
-		
-		panel.setBounds(69, 16, 267, 116);
-		panel_centro.add(panel);
-		
+
 		initialize();
 	}
 
@@ -78,22 +57,60 @@ public class VentanaInicio extends JFrame implements ActionListener{
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new BorderLayout(0, 0));
-		frame.setTitle("Questions4millions");
-		
-		JPanel panel = new JPanel();
-		frame.getContentPane().add(panel, BorderLayout.CENTER);
+
+		getContentPane().setLayout(null);
+
+		panel_centro = new JPanel();
+		getContentPane().add(panel_centro, BorderLayout.CENTER);
+		panel_centro.setLayout(null);
+
+		btnJugar = new JButton("Jugar");
+		btnJugar.setBounds(47, 154, 150, 29);
+		panel_centro.add(btnJugar);
+		btnJugar.addActionListener(this);
+
+		btnMarcadores = new JButton("Marcadores");
+		btnMarcadores.setBounds(47, 199, 150, 29);
+		panel_centro.add(btnMarcadores);
+
+		btnInstrucciones = new JButton("Instrucciones");
+		btnInstrucciones.setBounds(212, 154, 150, 29);
+		panel_centro.add(btnInstrucciones);
+
+		btnSalir = new JButton("Salir");
+		btnSalir.setBounds(212, 199, 150, 29);
+		panel_centro.add(btnSalir);
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		JButton source = (JButton) e.getSource();
-		if(source.equals(btnJugar)){
-			
+		if (source.equals(btnJugar)) {
+			java.awt.EventQueue.invokeLater(new Runnable() {
+
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					VentanaConfiguracion frame = new VentanaConfiguracion();
+					// frame.setSize(640, 640);
+					frame.setVisible(true);
+				}
+
+			});
+		} else if (source.equals(btnInstrucciones)) {
+			java.awt.EventQueue.invokeLater(new Runnable() {
+
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					VentanaConfiguracion frame = new VentanaConfiguracion();
+					// frame.setSize(640, 640);
+					frame.setVisible(true);
+				}
+
+			});
 		}
 	}
 }
