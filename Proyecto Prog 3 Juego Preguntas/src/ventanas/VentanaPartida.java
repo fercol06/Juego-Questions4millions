f257093682c542065e3fd8b4a978184f80f8f754
Javi <javi.fernandez@opendeusto.es>
2016-10-24 08:21:16 +0200

    VentanaPartida v0.0.1

--- Proyecto Prog 3 Juego Preguntas/src/ventanas/VentanaPartida.java
+++ Proyecto Prog 3 Juego Preguntas/src/ventanas/VentanaPartida.java
@@ -6,6 +6,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
 
 public class VentanaPartida extends JFrame {
 
@@ -37,6 +38,27 @@ public class VentanaPartida extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JPanel panelNorte = new JPanel();
+		contentPane.add(panelNorte, BorderLayout.NORTH);
+		
+		JPanel panelSur = new JPanel();
+		contentPane.add(panelSur, BorderLayout.SOUTH);
+		
+		JButton btnLanzarPregunta = new JButton("Lanzar Pregunta");
+		panelSur.add(btnLanzarPregunta);
+		
+		JButton btnTerminarJuego = new JButton("Finalizar Partida");
+		panelSur.add(btnTerminarJuego);
+		
+		JPanel panelIzquierda = new JPanel();
+		contentPane.add(panelIzquierda, BorderLayout.WEST);
+		
+		JPanel panelCentro = new JPanel();
+		contentPane.add(panelCentro, BorderLayout.CENTER);
+		
+		JPanel panelDerecha = new JPanel();
+		contentPane.add(panelDerecha, BorderLayout.EAST);
 	}
 
 }
