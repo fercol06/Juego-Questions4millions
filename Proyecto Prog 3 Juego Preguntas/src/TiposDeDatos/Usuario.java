0866c3f9d9b7f9bb3f0fde0a3ba484571ad7a2da
Javi <javi.fernandez@opendeusto.es>
2016-10-25 12:40:26 +0200

    Usuario

--- Proyecto Prog 3 Juego Preguntas/src/TiposDeDatos/Usuario.java
+++ Proyecto Prog 3 Juego Preguntas/src/TiposDeDatos/Usuario.java
@@ -6,5 +6,84 @@ public class Usuario {
 	private String email;
 	private String con;
 	private static int record;
+	
+	
+	/**
+	 * Contructor con parametros
+	 * @param user
+	 * @param email
+	 * @param con
+	 */
+	
+	public Usuario(String user, String email, String con) {
+		this.user = user;
+		this.email = email;
+		this.con = con;
+	}
+	/**
+	 * Contructor sin parametros
+	 */
+	
+	public Usuario() {
+		
+	}
+	
+	/**
+	 * @return the user
+	 */
+	public String getUser() {
+		return user;
+	}
+	/**
+	 * @param user the user to set
+	 */
+	public void setUser(String user) {
+		this.user = user;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the con
+	 */
+	public String getCon() {
+		return con;
+	}
+	/**
+	 * @param con the con to set
+	 */
+	public void setCon(String con) {
+		this.con = con;
+	}
+	/**
+	 * @return the record
+	 */
+	public static int getRecord() {
+		return record;
+	}
+	/**
+	 * @param record the record to set
+	 */
+	public static void setRecord(int record) {
+		Usuario.record = record;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Usuario [user=" + user + ", email=" + email + ", con=" + con + "]";
+	}
+	
 
 }
