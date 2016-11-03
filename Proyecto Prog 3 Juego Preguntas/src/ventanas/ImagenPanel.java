1440665fa9fc987b7437e4cdc93c2158f13b95c9
Javi <javi.fernandez@opendeusto.es>
2016-11-03 09:36:59 +0100

    imagen

--- Proyecto Prog 3 Juego Preguntas/src/ventanas/ImagenPanel.java
+++ Proyecto Prog 3 Juego Preguntas/src/ventanas/ImagenPanel.java
@@ -18,8 +18,8 @@ public class ImagenPanel extends JPanel {
 	protected void paintComponent (Graphics g){
 		Dimension d = getSize();
 		imagenTiempo=new ImageIcon(getClass().getResource(nombreImagen));
-		//g.drawImage(imagenTiempo.getImage(), 0, 0, d.width,d.height,null);
-		g.drawImage(imagenTiempo.getImage(), 0, 0, this.getWidth(),this.getHeight(),null);
+		g.drawImage(imagenTiempo.getImage(), 0, 0, d.width,d.height,null);
+		//g.drawImage(imagenTiempo.getImage(), 0, 0, this.getWidth(),this.getHeight(),null);
 		this.setOpaque(false);
 		super.paintComponent(g);
 	}
