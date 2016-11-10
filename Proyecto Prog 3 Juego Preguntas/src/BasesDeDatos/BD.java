a67888e8ce6f0cd88d9027435123fd2ab503c791
Alejandro Ron <alejandro.ron@opendeusto.es>
2016-11-10 09:30:39 +0100

    BD + Partida

--- Proyecto Prog 3 Juego Preguntas/src/BasesDeDatos/BD.java
+++ Proyecto Prog 3 Juego Preguntas/src/BasesDeDatos/BD.java
@@ -5,20 +5,20 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import TiposDeDatos.Pregunta;
 import TiposDeDatos.Usuario;
 
 public class BD {
-	
+
 	private Connection con;
 	private static Statement stmt;
-	
+
 	/**
-	 * Metodo que crea una sentencia para acceder a la base de datos 
+	 * Metodo que crea una sentencia para acceder a la base de datos
 	 */
-	public void crearSentencia()
-	{
+	public void crearSentencia() {
 		try {
 			stmt = con.createStatement();
 		} catch (SQLException e) {
@@ -32,24 +32,21 @@ public class BD {
 	 * Metodo establecer la conexión a la base de datos
 	 */
 
-	public void conectar()
-	{
+	public void conectar() {
 		try {
 			Class.forName("org.sqlite.JDBC");
-			con= DriverManager.getConnection("jdbc:sqlite:Q4M.db");
+			con = DriverManager.getConnection("jdbc:sqlite:Q4M.db");
 			crearSentencia();
-		}catch(Exception e)
-		{
+		} catch (Exception e) {
 			System.out.println("No se ha podido conectar a la base de datos");
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
-	 * Metodo para cerrar una sentencia 
+	 * Metodo para cerrar una sentencia
 	 */
-	public void cerrarSentencia()
-	{
+	public void cerrarSentencia() {
 		try {
 			stmt.close();
 		} catch (SQLException e) {
@@ -61,8 +58,7 @@ public class BD {
 	/**
 	 * Metodo para desconectarse de la base de datos
 	 */
-	public void desconectar()
-	{
+	public void desconectar() {
 		try {
 			cerrarSentencia();
 			con.close();
@@ -71,47 +67,64 @@ public class BD {
 			e.printStackTrace();
 		}
 	}
+
 	/**
 	 * Constructor de la base de datos. Se establece la conexión dentro.
 	 */
-	
-	public BD(){
+
+	public BD() {
 		conectar();
 	}
-	
+
 	/**
 	 * Método para obtener preguntas
 	 */
-	public Pregunta obtenerPregunta(){
-		Pregunta p=null;
-		//Preparamos la query
-		String query="SELECT * FROM preguntas"; 
+	public ArrayList<Pregunta> obtenerPregunta() {
+		Pregunta p = null;
+		ArrayList<Pregunta> aP = new ArrayList<>();
+		// Preparamos la query
+		String query = "SELECT * FROM preguntas";
 		try {
 			ResultSet rs = stmt.executeQuery(query);
-			// Comprobamos si ha devuelto filas
-			if(rs.next()) 
-				p=new Pregunta(rs.getString("pregunta"),rs.getString("resp1"),rs.getString("resp2"),rs.getString("resp3"),rs.getString("resp4"),rs.getString("respOk"),rs.getInt("nivel"));
+			while (rs.next()) {
+				// Comprobamos si ha devuelto filas
+				p = new Pregunta(rs.getString("pregunta"), rs.getString("resp1"), rs.getString("resp2"),
+						rs.getString("resp3"), rs.getString("resp4"), rs.getString("respOk"), rs.getInt("nivel"));
+				aP.add(p);
+			}
+
 			rs.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		return p;
+		return aP;
 	}
+
 	/**
 	 * Método para añadir pregunta
 	 */
-	public void aniadirPregunta(Pregunta p){
-		
-		/*Preparamos la query
+	public void aniadirPregunta(Pregunta p) {
+
+		/*
+		 * Preparamos la query
+		 * 
 		 * @param pregunta
+		 * 
 		 * @param resp1
+		 * 
 		 * @param resp2
+		 * 
 		 * @param resp3
+		 * 
 		 * @param resp4
+		 * 
 		 * @param respCorrecta
+		 * 
 		 * @param nivel
 		 */
-		String query="INSERT INTO preguntas (pregunta,resp1,resp2,resp3,resp4,respOk,nivel) VALUES ('"+p.getPregunta()+"','"+p.getResp1()+"',"+p.getResp2()+",'"+p.getResp3()+",'"+p.getResp4()+"','"+p.getRespCorrecta()+",'"+p.getNivel()+"')"; 
+		String query = "INSERT INTO preguntas (pregunta,resp1,resp2,resp3,resp4,respOk,nivel) VALUES ('"
+				+ p.getPregunta() + "','" + p.getResp1() + "'," + p.getResp2() + ",'" + p.getResp3() + ",'"
+				+ p.getResp4() + "','" + p.getRespCorrecta() + ",'" + p.getNivel() + "')";
 		try {
 			stmt.executeUpdate(query);
 		} catch (SQLException e) {
@@ -119,31 +132,30 @@ public class BD {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Método para obtener Usuario Admin
+	 * 
 	 * @param dni
 	 * @return
 	 */
-	
-	public Usuario obtenerUsuarioAdmin(String nombre){
+
+	public Usuario obtenerUsuarioAdmin(String nombre) {
 		String query;
-		Usuario u=null;
-		//Preparamos la query
-		query="SELECT * FROM usuario WHERE user='"+nombre+"' AND tipo='admin'"; 
+		Usuario u = null;
+		// Preparamos la query
+		query = "SELECT * FROM usuario WHERE user='" + nombre + "' AND tipo='admin'";
 		try {
 			ResultSet rs = stmt.executeQuery(query);
 			// Comprobamos si ha devuelto filas
-			if(rs.next()) 
-				u=new Usuario(rs.getString("user"),rs.getString("email"),rs.getString("pass"),rs.getInt("record"),rs.getString("tipo"));
+			if (rs.next())
+				u = new Usuario(rs.getString("user"), rs.getString("email"), rs.getString("pass"), rs.getInt("record"),
+						rs.getString("tipo"));
 			rs.close();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return u;
 	}
-	 
-
-
 
 }
--- Proyecto Prog 3 Juego Preguntas/src/TiposDeDatos/Partida.java
+++ Proyecto Prog 3 Juego Preguntas/src/TiposDeDatos/Partida.java
@@ -1,5 +1,34 @@
 package TiposDeDatos;
 
+import java.util.ArrayList;
+
 public class Partida {
+
+	
+
+	private ConfiguracionJuego config;
+	private ArrayList<Usuario> aUsuario;
+	// private ArrayList<Pregunta> aPregunta;
+
+	public Partida(ConfiguracionJuego config, ArrayList<Usuario> aUsuario) {
+		this.config = config;
+		this.aUsuario = aUsuario;
+	}
+
+	public ConfiguracionJuego getConfig() {
+		return config;
+	}
+
+	public void setConfig(ConfiguracionJuego config) {
+		this.config = config;
+	}
+
+	public ArrayList<Usuario> getaUsuario() {
+		return aUsuario;
+	}
+
+	public void setaUsuario(ArrayList<Usuario> aUsuario) {
+		this.aUsuario = aUsuario;
+	}
 
 }
